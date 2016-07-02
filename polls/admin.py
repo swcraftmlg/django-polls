f32cfeacff40a694e1be7d12867a14f5256feb1c
Javier Matos Odut <iam@javiermatos.com>
2016-07-02 19:29:38 +0200

    Extendido el modelo de datos. Corregido el incremento de votos para evitar problemas por concurrencia. Agregadas fixtures para tener datos de partida. Agregado comando para cerrar una votación. Mejorada la estructuración de las configuraciones del archivo settings.py.

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.utils import timezone
 
-from .models import Choice, Question
+from .models import Question, Choice
 
 
 class ChoiceInline(admin.TabularInline):
@@ -9,14 +10,25 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ['question_text', 'pub_date', 'was_published_recently']
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+    list_display = ['text', 'published', 'closed', 'votes']
+    list_filter = ['published']
+    search_fields = ['text']
 
     fieldsets = [
-        (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        (None, {'fields': ['text']}),
+        ('Date information', {
+            'fields': ['published', 'closed'],
+            'classes': ['collapse'],
+        }),
     ]
     inlines = [ChoiceInline]
+
+    actions = ['make_close']
+
+    def make_close(self, request, queryset):
+        now = timezone.now()
+        queryset.filter(published__lte=now, closed__isnull=True).update(closed=now)
+
+    make_close.short_description = 'Close selected questions'
 
 admin.site.register(Question, QuestionAdmin)
--- polls/views.py
+++ polls/views.py
@@ -4,27 +4,49 @@ from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from django.views import generic
 
+from . import constants
+from . import exceptions
 from .models import Question, Choice
 
 
-class IndexView(generic.ListView):
+class ListView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+    paginate_by = constants.PAGE_SIZE
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now(), choice__isnull=False).distinct().order_by('-pub_date')[:5]
+        """
+        Return the latest questions.
+        """
+        queryset = Question.objects.filter(published__lte=timezone.now())  # Show published
+        queryset = queryset.filter(choice__isnull=False).distinct()  # Remove questions without choices
+        queryset = queryset.order_by('-published', '-closed', '-id')  # Order by published and closed datetime, and id
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        """
+        Extend context to pass page_kwarg value instead of hard coding it.
+        """
+        context = {'page_kwarg': self.page_kwarg}
+        context.update(kwargs)
+        return super(ListView, self).get_context_data(**context)
 
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
 
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(published__lte=timezone.now())
+
+    def get_template_names(self):
+        question = self.get_object()
+        template_name = 'polls/detail.html'
+        if question.closed is not None and question.closed < timezone.now():
+            template_name = 'polls/results.html'
+        return template_name
 
 
 class ResultsView(generic.DetailView):
@@ -35,23 +57,24 @@ class ResultsView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(published__lte=timezone.now())
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choices.get(pk=request.POST['choice'])
+        selected_choice.increment_votes()
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    except exceptions.ChoiceVotingError:
+        pass
+
+    # Always return an HttpResponseRedirect after successfully dealing with
+    # POST data. This prevents data from being posted twice if a user hits the
+    # Back button.
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
